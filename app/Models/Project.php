d1c42883a90f99973464b667abaf38bb4c36f6bf
RaimondoCosma <raimondo.cosma@gmail.com>
2023-02-02 00:04:03 +0100

    Aggiunge placeholder se immagine non presente

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,6 +10,12 @@ class Project extends Model
     use HasFactory;
 
     protected $guarded = ['slug'];
+    protected $appends = ['image_url'];
+
+    protected function getImageUrlAttribute()
+    {
+        return $this->thumb ? asset("storage/$this->thumb") : "https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg";
+    }
 
     public function type()
     {
